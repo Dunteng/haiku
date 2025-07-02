a6a55233f6d5583831a2d8a3444c7bae3726a67b
daltonllin <daltonllin@tencent.com>
2025-07-02 20:36:31 +0800

    feat: 优化日式和风俳句生成器界面交互体验 --story=1001

---
+++ src/ai/index.js
@@ -0,0 +1,22 @@
+      
+// Please install OpenAI SDK first: `npm install openai`
+
+import OpenAI from "openai";
+
+const openai = new OpenAI({
+        baseURL: 'https://api.deepseek.com',
+        apiKey: 'sk-0a183ae4cbb1446facfc99e37576886b'
+});
+
+async function main() {
+  const completion = await openai.chat.completions.create({
+    messages: [{ role: "system", content: "You are a helpful assistant." }],
+    model: "deepseek-chat",
+  });
+
+  console.log(completion.choices[0].message.content);
+}
+
+main();
+
+    
